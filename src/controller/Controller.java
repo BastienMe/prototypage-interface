05e1d4bb580380d7a8feca652f6711dd1f44f3dd
Bastien <Bastien@DESKTOP-TKPOTM1.home>
2022-04-02 17:13:57 +0200

    Mise en forme des messages hote et voyageur

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import controller.Controller;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -28,10 +29,13 @@ import javafx.stage.Stage;
 //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
 public class Controller{
 	
-	@FXML TextField textField;
+	@FXML TextField textFieldChat;
 	@FXML VBox vBoxChat;
 	@FXML AnchorPane anchorPaneChat;
 	@FXML ScrollPane scrollPaneChat;
+	
+	boolean hote = false;
+	
 	public void OnContacterClicked(ActionEvent event) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/Discussion.fxml"));
 		Parent root = (Parent) loader.load();
@@ -50,22 +54,56 @@ public class Controller{
 	}
 	
 	public void OnEnvoyerMessageClicked(ActionEvent event) throws IOException {
-		
-		System.out.print(textField.getText());
-		Text text = new Text(textField.getText());
-		text.setWrappingWidth(150);
-		BorderPane borderPane = new BorderPane();
-		BorderPane borderPane1 = new BorderPane();
-		borderPane.setRight(borderPane1);
-		borderPane1.setRight(text);
-		borderPane1.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 5px;");
-		System.out.println(text.getLayoutBounds().getWidth());
-		//vBoxChat.setSpacing(text.getBoundsInParent().getHeight() + 10);
-		
-		vBoxChat.getChildren().add(borderPane);
-		vBoxChat.autosize();
-		
-		anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
-		scrollPaneChat.setVvalue(1.0);  
+		//scrollPaneChat.setVvalue(1.0);  
+		if(textFieldChat.getText().length() != 0) {
+			System.out.print(textFieldChat.getText());
+			String textFinal = textFieldChat.getText();
+			textFieldChat.clear();
+			
+			/*int newLineCpt = textField.getText().length()/20;
+			if(textField.getText().length()>20) {
+				textFinal = "";
+				int j = 0;
+				for(int i = 0; i < textField.getText().length(); i++) {
+					if(i == 20 && j == 0) {
+						textFinal += "\n";
+						j = 2;
+					}
+					textFinal += textField.getText().charAt(i);
+					
+				}
+				
+			}*/
+			Text text = new Text(textFinal);
+			text.setWrappingWidth(150);
+			BorderPane borderPane = new BorderPane();
+			borderPane.setPrefWidth(295);
+			BorderPane borderPane1 = new BorderPane();
+			if(!hote) {
+				borderPane.setLeft(borderPane1);
+				borderPane1.setLeft(text);
+				
+				borderPane1.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 5px;");
+				hote = !hote;
+			} else {
+				borderPane.setRight(borderPane1);
+				borderPane1.setRight(text);
+				
+				borderPane1.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
+				hote = !hote;
+			}
+			System.out.println(text.getLayoutBounds().getWidth());
+			//vBoxChat.setSpacing(text.getBoundsInParent().getHeight() + 10);
+			
+			vBoxChat.getChildren().add(borderPane);
+			vBoxChat.autosize();
+			
+			anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
+			scrollPaneChat.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+			//scrollPaneChat.setVvalue(1.0);  
+			
+			
+			scrollPaneChat.vvalueProperty().bind(vBoxChat.heightProperty());
+		}
 	}
 }
