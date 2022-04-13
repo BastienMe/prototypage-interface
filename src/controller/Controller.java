bc233f5c33af24ec69abe6f1f69f9f96ef12227b
meffrebastien84@Gmail.com <meffrebastien84@Gmail.com>
2022-04-13 14:39:50 +0200

    Implémentation validation/acceptation voyage

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -18,6 +18,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -33,6 +34,7 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 //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
 //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
+//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
 public class Controller{
 	
 	@FXML TextField textFieldChat;
@@ -40,10 +42,14 @@ public class Controller{
 	@FXML AnchorPane anchorPaneChat;
 	@FXML ScrollPane scrollPaneChat;
 	@FXML VBox vBoxToutMessagerie;
+	@FXML Button buttonVoyageurValiderVoyage;
+	@FXML Button buttonHoteValiderVoyage;
+	@FXML Button buttonRefuserVoyage;
 	boolean hote = false;
 	
+	
 	public void OnContacterClicked(ActionEvent event) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/Discussion.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/DiscussionVoyageur.fxml"));
 		Parent root = (Parent) loader.load();
 		Controller secController = loader.getController();
 		secController.lireFichierChat(12,344);
@@ -51,12 +57,18 @@ public class Controller{
 		//secController.easyModel = easyModel;
 		
 		Stage stage = new Stage();
+		stage.setTitle("Communication");
 		stage.initModality(Modality.APPLICATION_MODAL);  
 		stage.setScene(new Scene(root));
 		stage.show();
 
 	}
-	
+	public void onConnexionClientClicked() {
+		hote = false;
+	}
+	public void onConnexionHoteClicked() {
+		hote = true;
+	}
 	public void OnEnvoyerMessageClicked(ActionEvent event) throws IOException {
 		int idVoyage = 12;
 		int idUtilisateur = 344;
@@ -68,23 +80,22 @@ public class Controller{
 			BorderPane borderPane = new BorderPane();
 			borderPane.setPrefWidth(295);
 			BorderPane borderPane1 = new BorderPane();
-			if(!hote) {
+			//if(!hote) {
 				borderPane.setLeft(borderPane1);
 				borderPane1.setLeft(text);
 				
 				borderPane1.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 5px;");
 				creeFichierChat(idVoyage,idUtilisateur);
 				ecrireFichierChat(idVoyage,idUtilisateur,hote,textFinal);
-				hote = !hote;
-			} else {
+				/*}else {
 				borderPane.setRight(borderPane1);
 				borderPane1.setRight(text);
 				
 				borderPane1.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
 				creeFichierChat(idVoyage,idUtilisateur);
 				ecrireFichierChat(idVoyage,idUtilisateur,hote,textFinal);
-				hote = !hote;
-			}
+			}*/
+			
 			
 			vBoxChat.getChildren().add(borderPane);
 			vBoxChat.autosize();
@@ -137,11 +148,27 @@ public class Controller{
 		      while (scanner.hasNextLine()) {
 		        String data = scanner.nextLine();
 		        if(ligne > 1) {
-			        if(data.charAt(0) == '*') {
-			        	StringBuilder stringBuilder = new StringBuilder(data);
-			        	stringBuilder.deleteCharAt(0);
-			        	setChat(true,stringBuilder.toString());
-			        } else setChat(false,data);
+			        if(data.charAt(0) == '*') { // Message de l'hote
+			        	if(data.contains("|vh|")) {
+			        		StringBuilder stringBuilder = new StringBuilder(data);
+				        	stringBuilder.delete(0,5);
+				        	setValidation("|vh|",stringBuilder.toString());
+			        	} else if(data.contains("|rh|")) {
+			        		StringBuilder stringBuilder = new StringBuilder(data);
+				        	stringBuilder.delete(0,5);
+				        	setValidation("|rh|",stringBuilder.toString());
+			        	} else {
+				        	StringBuilder stringBuilder = new StringBuilder(data);
+				        	stringBuilder.deleteCharAt(0);
+				        	setChat(true,stringBuilder.toString());
+			        	}
+			        }else if(data.contains("|vv|")){ // Message du voyageur
+			        	if(!hote) buttonVoyageurValiderVoyage.setDisable(true);
+			        	StringBuilder stringBuilder1 = new StringBuilder(data);
+			        	stringBuilder1.delete(0, 4);
+			        	setValidation("|vv|",stringBuilder1.toString());
+			        }else setChat(false,data);
+			        
 		        }
 		        ligne += 1;
 		      }
@@ -150,34 +177,102 @@ public class Controller{
 	    } catch (FileNotFoundException e) {
 	      e.printStackTrace();
 	    }
-		
+		anchorPaneChat.setPrefHeight(vBoxChat.getHeight()+30);
+		scrollPaneChat.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+		scrollPaneChat.setVvalue(1.0); 
 	}
 	
-	public void setChat(boolean hote, String message) {
+	public void setChat(boolean messageHote, String message) {
+		
 		Text text = new Text(message);
 		text.setWrappingWidth(150);
 		BorderPane borderPane = new BorderPane();
 		borderPane.setPrefWidth(295);
 		BorderPane borderPane1 = new BorderPane();
-		if(!hote) {
-			borderPane.setLeft(borderPane1);
-			borderPane1.setLeft(text);
-			
-			borderPane1.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 5px;");
-			hote = !hote;
+		if(!messageHote) {
+			if(!hote) {
+				borderPane.setLeft(borderPane1);
+				borderPane1.setLeft(text);
+				
+				borderPane1.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 5px;");
+			} else {
+				borderPane.setRight(borderPane1);
+				borderPane1.setRight(text);
+				
+				borderPane1.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
+			}
 		} else {
-			borderPane.setRight(borderPane1);
-			borderPane1.setRight(text);
-			
-			borderPane1.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
-			hote = !hote;
+			if(hote) {
+				borderPane.setLeft(borderPane1);
+				borderPane1.setLeft(text);
+				
+				borderPane1.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 5px;");
+			} else {
+				borderPane.setRight(borderPane1);
+				borderPane1.setRight(text);
+				
+				borderPane1.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
+			}
 		}
 		vBoxChat.getChildren().add(borderPane);
 		vBoxChat.autosize();
 		
-		anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
+		anchorPaneChat.setPrefHeight(vBoxChat.getHeight()+10);
 		scrollPaneChat.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
 		scrollPaneChat.setVvalue(1.0); 
+	}
+	
+	public void setValidation(String code, String message) {
+		if(code.equals("|vv|") || code.equals("|vh|")) {
+			Text text = new Text(message);
+			text.setWrappingWidth(150);
+			BorderPane borderPane = new BorderPane();
+			borderPane.setPrefWidth(295);
+			BorderPane borderPane1 = new BorderPane();
+		
+			borderPane.setCenter(borderPane1);
+			borderPane1.setCenter(text);
+			
+			borderPane1.setStyle("-fx-background-color: #0DFF00; -fx-background-radius: 5px;");
+			
+			vBoxChat.getChildren().add(borderPane);
+			vBoxChat.autosize();
+			
+			anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
+			scrollPaneChat.setVvalue(1.0); 
+			
+			
+			
+			// Debloque les bouton de l'hote
+			if(buttonHoteValiderVoyage != null) {
+				buttonHoteValiderVoyage.setDisable(false);
+				buttonRefuserVoyage.setDisable(false);
+			}
+		
+			
+		} else if(code.equals("|rh|")) {
+			
+			Text text = new Text(message);
+			text.setWrappingWidth(150);
+			BorderPane borderPane = new BorderPane();
+			borderPane.setPrefWidth(295);
+			BorderPane borderPane1 = new BorderPane();
+		
+			borderPane.setCenter(borderPane1);
+			borderPane1.setCenter(text);
+			
+			borderPane1.setStyle("-fx-background-color: #FF0000; -fx-background-radius: 5px;");
+			
+			vBoxChat.getChildren().add(borderPane);
+			vBoxChat.autosize();
+			
+			anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
+			scrollPaneChat.setVvalue(1.0); 
+			
+		
+			if(buttonRefuserVoyage != null)	buttonRefuserVoyage.setDisable(true);
+			if(buttonVoyageurValiderVoyage != null)	buttonVoyageurValiderVoyage.setDisable(false);
+		}
 	}
 	
 	public void onMessagerieClicked() throws IOException {
@@ -188,7 +283,9 @@ public class Controller{
 		//secController.net = net;
 		//secController.easyModel = easyModel;
 		secController.setUpMessagerie();
+		secController.hote = hote;
 		Stage stage = new Stage();
+		stage.setTitle("Messagerie");
 		stage.initModality(Modality.APPLICATION_MODAL);  
 		stage.setScene(new Scene(root));
 		stage.show();
@@ -229,6 +326,11 @@ public class Controller{
 			        else if(ligne == 1) nomUtilisateur = data;
 			        else {
 			        	StringBuilder stringBuilder1 = new StringBuilder(data);
+			        	if(data.contains("|vv|")) {
+			        		stringBuilder1.delete(0, 4);
+			        	} else if(data.contains("|vh|") || data.contains("|rh|")) {
+			        		stringBuilder1.delete(0, 5);
+			        	}
 			        	if(stringBuilder1.charAt(0) == '*')
 			        		stringBuilder1.deleteCharAt(0);
 			        	dernierMessage = stringBuilder1.toString();
@@ -250,16 +352,22 @@ public class Controller{
 			vBoxMessagerie.getChildren().add(textDernierMessage);
 			vBoxMessagerie.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
 			vBoxMessagerie.setOnMouseClicked(event -> {
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/Discussion.fxml"));
+				FXMLLoader loader = null;
+				
+				if(hote) 
+					loader = new FXMLLoader(getClass().getResource("../views/DiscussionHote.fxml"));
+				else 
+					loader = new FXMLLoader(getClass().getResource("../views/DiscussionVoyageur.fxml"));
 				Parent root;
 				try {
 					root = (Parent) loader.load();
 					Controller secController = loader.getController();
+					
+					secController.hote = hote;
 					secController.lireFichierChat(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]));
-					//secController.net = net;
-					//secController.easyModel = easyModel;
 					
 					Stage stage = new Stage();
+					stage.setTitle("Communication");
 					stage.initModality(Modality.APPLICATION_MODAL);  
 					stage.setScene(new Scene(root));
 					stage.show();
@@ -271,6 +379,86 @@ public class Controller{
 			});
 			vBoxToutMessagerie.getChildren().add(vBoxMessagerie);
 	    }
-		
 	}
+	
+	public void onVoyageurValiderVoyageClicked() {
+		buttonVoyageurValiderVoyage.setDisable(true);
+		int idVoyage = 12;
+		int idUtilisateur = 344;
+		//buttonHoteValiderVoyage.setDisable(false);
+		Text text = new Text("Voyage validé par le voyageur");
+		text.setWrappingWidth(150);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setPrefWidth(295);
+		BorderPane borderPane1 = new BorderPane();
+	
+		borderPane.setCenter(borderPane1);
+		borderPane1.setCenter(text);
+		
+		borderPane1.setStyle("-fx-background-color: #0DFF00; -fx-background-radius: 5px;");
+		creeFichierChat(idVoyage,idUtilisateur);
+		ecrireFichierChat(idVoyage,idUtilisateur,false,"|vv|Voyage validé par le voyageur");
+	
+		vBoxChat.getChildren().add(borderPane);
+		vBoxChat.autosize();
+		
+		anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
+		scrollPaneChat.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+		
+		scrollPaneChat.vvalueProperty().bind(vBoxChat.heightProperty());
+	}
+	
+	public void onHoteValiderVoyageClicked() {
+		buttonHoteValiderVoyage.setDisable(true);
+		int idVoyage = 12;
+		int idUtilisateur = 344;
+		//buttonHoteValiderVoyage.setDisable(false);
+		Text text = new Text("Voyage validé par l'hôte");
+		text.setWrappingWidth(150);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setPrefWidth(295);
+		BorderPane borderPane1 = new BorderPane();
+	
+		borderPane.setCenter(borderPane1);
+		borderPane1.setCenter(text);
+		
+		borderPane1.setStyle("-fx-background-color: #0DFF00; -fx-background-radius: 5px;");
+		creeFichierChat(idVoyage,idUtilisateur);
+		ecrireFichierChat(idVoyage,idUtilisateur,true,"|vh|Voyage validé par l'hôte");
+	
+		vBoxChat.getChildren().add(borderPane);
+		vBoxChat.autosize();
+		
+		anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
+		scrollPaneChat.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+		
+		scrollPaneChat.vvalueProperty().bind(vBoxChat.heightProperty());
+	}
+	public void onRefuserVoyageClicked() {
+		buttonRefuserVoyage.setDisable(true);
+		int idVoyage = 12;
+		int idUtilisateur = 344;
+		//buttonHoteValiderVoyage.setDisable(false);
+		Text text = new Text("Voyage refusé par l'hôte");
+		text.setWrappingWidth(150);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setPrefWidth(295);
+		BorderPane borderPane1 = new BorderPane();
+	
+		borderPane.setCenter(borderPane1);
+		borderPane1.setCenter(text);
+		
+		borderPane1.setStyle("-fx-background-color: #FF0000; -fx-background-radius: 5px;");
+		creeFichierChat(idVoyage,idUtilisateur);
+		ecrireFichierChat(idVoyage,idUtilisateur,true,"|rh|Voyage refusé par l'hôte");
+		
+		vBoxChat.getChildren().add(borderPane);
+		vBoxChat.autosize();
+		
+		anchorPaneChat.setPrefHeight(vBoxChat.getHeight());
+		scrollPaneChat.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+		
+		scrollPaneChat.vvalueProperty().bind(vBoxChat.heightProperty());
+	}
+	
 }
