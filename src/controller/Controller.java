13bdd1da27b8b9f3ad8c096f3e482f20944b101c
meffrebastien84@Gmail.com <meffrebastien84@Gmail.com>
2022-04-26 12:59:10 +0200

    Refactor

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,8 +20,10 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -45,21 +48,55 @@ public class Controller{
 	@FXML Button buttonVoyageurValiderVoyage;
 	@FXML Button buttonHoteValiderVoyage;
 	@FXML Button buttonRefuserVoyage;
+	@FXML MenuButton buttonMenu;
 	boolean hote = false;
+	int idVoyage = 0;
+	int idVoyageur = 7;
+	int idHote = 1;
+	int idUtilisateurConnecte = 7;
 	
-	
+	public void onMenuButtonProfilClicked(ActionEvent event) {
+		
+	}
+	public void onMenuButtonMessagerieClicked(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/ListeDiscussions.fxml"));
+		Parent root = (Parent) loader.load();
+		Controller secController = loader.getController();
+		
+		//secController.net = net;
+		//secController.easyModel = easyModel;
+		secController.hote = hote;
+		secController.setUpMessagerie();
+		
+		Stage stage = new Stage();
+		stage.setTitle("Messagerie");
+		stage.initModality(Modality.APPLICATION_MODAL);  
+		stage.setScene(new Scene(root));
+		stage.show();
+	}
+	public void onMenuButtonDeconnexionClicked(ActionEvent event) {
+		
+	}
 	public void OnContacterClicked(ActionEvent event) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/DiscussionVoyageur.fxml"));
 		Parent root = (Parent) loader.load();
 		Controller secController = loader.getController();
-		secController.lireFichierChat(12,344);
+		secController.lireFichierChat(idVoyage,idVoyageur,idHote);
 		
 		Stage stage = new Stage();
 		stage.setTitle("Communication");
 		stage.initModality(Modality.APPLICATION_MODAL);  
 		stage.setScene(new Scene(root));
 		stage.show();
-
+		
+		ImageView imageUser = new ImageView("C:\\Users\\meffr\\git\\prototypage-interface\\User_icon_BLACK-01.png");
+		imageUser.setFitHeight(50);
+		imageUser.setFitWidth(50);
+		
+		buttonMenu.setText("");
+		buttonMenu.setGraphic(imageUser);
+	
+		
 	}
 	public void onConnexionClientClicked() {
 		hote = false;
@@ -68,8 +105,7 @@ public class Controller{
 		hote = true;
 	}
 	public void OnEnvoyerMessageClicked(ActionEvent event) throws IOException {
-		int idVoyage = 12;
-		int idUtilisateur = 344;
+		
 		
 		if(textFieldChat.getText().length() != 0) {
 			String textFinal = textFieldChat.getText();
@@ -83,8 +119,8 @@ public class Controller{
 			borderPane1.setLeft(text);
 			
 			borderPane1.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 5px;");
-			creeFichierChat(idVoyage,idUtilisateur);
-			ecrireFichierChat(idVoyage,idUtilisateur,hote,textFinal);
+			creeFichierChat(idVoyage,idVoyageur,idHote);
+			ecrireFichierChat(idVoyage,idVoyageur,idHote,hote,textFinal);
 		
 			
 			vBoxChat.getChildren().add(borderPane);
@@ -99,14 +135,15 @@ public class Controller{
 		}
 	}
 	
-	public void creeFichierChat(int idVoyage, int idUtilisateur) {
+	public void creeFichierChat(int idVoyage, int idVoyageur, int idHote) {
 		try {
-	      File file = new File("Discussions/"+ idVoyage + "_"+ idUtilisateur +".txt");
+	      File file = new File("Discussions/"+ idVoyage + "_"+ idVoyageur +"_"+idHote+".txt");
 	      if(!file.exists()) {
 	    	  file.createNewFile();
-		      FileWriter fileWriter = new FileWriter("Discussions/"+ idVoyage + "_"+ idUtilisateur +".txt",true);
+		      FileWriter fileWriter = new FileWriter("Discussions/"+ idVoyage + "_"+ idVoyageur +"_"+idHote+".txt",true);
 		      fileWriter.write(idVoyage+"\n");
-		      fileWriter.write(idUtilisateur+"\n");
+		      fileWriter.write(idVoyageur+"\n");
+		      fileWriter.write(idHote+"\n");
 		      fileWriter.close();
 	      }
 	      
@@ -115,9 +152,9 @@ public class Controller{
 	    }
 	}
 	
-	public void ecrireFichierChat(int idVoyage, int idUtilisateur, boolean hote, String message) {
+	public void ecrireFichierChat(int idVoyage, int idUtilisateur, int idHote, boolean hote, String message) {
 		try {
-	      FileWriter fileWriter = new FileWriter("Discussions/"+ idVoyage+"_"+ idUtilisateur +".txt",true);
+	      FileWriter fileWriter = new FileWriter("Discussions/"+ idVoyage+"_"+ idUtilisateur +"_"+idHote+".txt",true);
 	      if(hote) {
 	    	  fileWriter.write("*"+message+"\n");
 	      } else {
@@ -129,15 +166,15 @@ public class Controller{
 	    }
 	}
 	
-	public void lireFichierChat(int idVoyage, int utilisateur) {
+	public void lireFichierChat(int idVoyage, int utilisateur, int idHote) {
 		try {
-	      File file = new File("Discussions/"+ idVoyage +"_"+ utilisateur +".txt");
+	      File file = new File("Discussions/"+ idVoyage +"_"+ idVoyageur+"_"+idHote +".txt");
 	      if(file.exists()) {
 		      Scanner scanner = new Scanner(file);
 		      int ligne = 0;
 		      while (scanner.hasNextLine()) {
 		        String data = scanner.nextLine();
-		        if(ligne > 1) {
+		        if(ligne > 2) {
 			        if(data.charAt(0) == '*') { // Message de l'hote
 			        	if(data.contains("|vh|")) {
 			        		StringBuilder stringBuilder = new StringBuilder(data);
@@ -293,13 +330,15 @@ public class Controller{
 		
 		//secController.net = net;
 		//secController.easyModel = easyModel;
-		secController.setUpMessagerie();
 		secController.hote = hote;
+		secController.setUpMessagerie();
+		
 		Stage stage = new Stage();
 		stage.setTitle("Messagerie");
 		stage.initModality(Modality.APPLICATION_MODAL);  
 		stage.setScene(new Scene(root));
 		stage.show();
+		
 	}
 	
 	public static ArrayList<String> readDiscussionsFolder() {
@@ -316,6 +355,7 @@ public class Controller{
 	}
 	
 	public void setUpMessagerie() {
+		
 		List<String> nomMessageries = readDiscussionsFolder();
 		for (String nomMessagerie : nomMessageries) {
 			//System.out.println(nomMessagerie);
@@ -326,8 +366,9 @@ public class Controller{
 			
 			int ligne = 0;
 			String nomVoyage ="";
-			String nomUtilisateur="";
+			String nomVoyageur="";
 			String dernierMessage="";
+			String nomHote="";
 			try {
 		      File file = new File("Discussions/"+nomMessagerie);
 		      if(file.exists()) {
@@ -335,7 +376,8 @@ public class Controller{
 			      while (scanner.hasNextLine()) {
 			        String data = scanner.nextLine();
 			        if(ligne == 0) nomVoyage = data;
-			        else if(ligne == 1) nomUtilisateur = data;
+			        else if(ligne == 1) nomVoyageur = data;
+			        else if(ligne == 2) nomHote = data;
 			        else {
 			        	StringBuilder stringBuilder1 = new StringBuilder(data);
 			        	if(data.contains("|vv|")) {
@@ -355,48 +397,60 @@ public class Controller{
 		      e.printStackTrace();
 		    }
 				
-			Text textNomVoyage = new Text(nomVoyage);
-			Text textNomUtilisateur = new Text(nomUtilisateur);
-			Text textDernierMessage = new Text(dernierMessage);
+			
 			VBox vBoxMessagerie = new VBox();
-			vBoxMessagerie.getChildren().add(textNomVoyage);
-			vBoxMessagerie.getChildren().add(textNomUtilisateur);
-			vBoxMessagerie.getChildren().add(textDernierMessage);
-			vBoxMessagerie.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
-			vBoxMessagerie.setOnMouseClicked(event -> {
-				FXMLLoader loader = null;
-				
-				if(hote) 
-					loader = new FXMLLoader(getClass().getResource("../views/DiscussionHote.fxml"));
-				else 
-					loader = new FXMLLoader(getClass().getResource("../views/DiscussionVoyageur.fxml"));
-				Parent root;
-				try {
-					root = (Parent) loader.load();
-					Controller secController = loader.getController();
-					
-					secController.hote = hote;
-					secController.lireFichierChat(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]));
-					
-					Stage stage = new Stage();
-					stage.setTitle("Communication");
-					stage.initModality(Modality.APPLICATION_MODAL);  
-					stage.setScene(new Scene(root));
-					stage.show();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			//System.out.println(idUtilisateurConnecte + " " + Integer.parseInt(nomVoyageur) + " " + Integer.parseInt(nomHote));
+			if((idUtilisateurConnecte == idVoyageur || idUtilisateurConnecte == idHote) && (idUtilisateurConnecte == Integer.parseInt(nomVoyageur) || idUtilisateurConnecte == Integer.parseInt(nomHote))) {
+				//System.out.println("OUI");
+				Text textNomVoyage = new Text("Voyage : "+nomVoyage);
+				Text textNomUtilisateur = new Text("");
+				if(idUtilisateurConnecte == idHote) {
+					hote = true;
+					textNomUtilisateur.setText("Voyageur : "+nomVoyageur);
 				}
+				else {
+					hote = false;
+					textNomUtilisateur.setText("Hote : "+nomHote);
+				}
+				Text textDernierMessage = new Text("Message : "+dernierMessage);
+				vBoxMessagerie.getChildren().add(textNomVoyage);
+				vBoxMessagerie.getChildren().add(textNomUtilisateur);
+				vBoxMessagerie.getChildren().add(textDernierMessage);
+				vBoxMessagerie.setStyle("-fx-background-color: #6e97f0; -fx-background-radius: 5px;");
 				
-			});
-			vBoxToutMessagerie.getChildren().add(vBoxMessagerie);
+				vBoxMessagerie.setOnMouseClicked(event -> {
+					FXMLLoader loader = null;
+					
+					if(hote) 
+						loader = new FXMLLoader(getClass().getResource("../views/DiscussionHote.fxml"));
+					else 
+						loader = new FXMLLoader(getClass().getResource("../views/DiscussionVoyageur.fxml"));
+					Parent root;
+					try {
+						root = (Parent) loader.load();
+						Controller secController = loader.getController();
+						
+						secController.hote = hote;
+						secController.lireFichierChat(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]),Integer.parseInt(tokens[2]));
+						
+						Stage stage = new Stage();
+						stage.setTitle("Communication");
+						stage.initModality(Modality.APPLICATION_MODAL);  
+						stage.setScene(new Scene(root));
+						stage.show();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				});
+				vBoxToutMessagerie.getChildren().add(vBoxMessagerie);
+			}
 	    }
 	}
 	
 	public void onVoyageurValiderVoyageClicked() {
 		buttonVoyageurValiderVoyage.setDisable(true);
-		int idVoyage = 12;
-		int idUtilisateur = 344;
 		//buttonHoteValiderVoyage.setDisable(false);
 		Text text = new Text("Voyage validé par le voyageur");
 		text.setWrappingWidth(250);
@@ -408,8 +462,8 @@ public class Controller{
 		borderPane1.setCenter(text);
 		
 		borderPane1.setStyle("-fx-background-color: #0DFF00; -fx-background-radius: 5px;");
-		creeFichierChat(idVoyage,idUtilisateur);
-		ecrireFichierChat(idVoyage,idUtilisateur,false,"|vv|Voyage validé par le voyageur");
+		creeFichierChat(idVoyage,idVoyageur,idHote);
+		ecrireFichierChat(idVoyage,idVoyageur,idHote,false,"|vv|Voyage validé par le voyageur");
 	
 		vBoxChat.getChildren().add(borderPane);
 		vBoxChat.autosize();
@@ -422,8 +476,7 @@ public class Controller{
 	
 	public void onHoteValiderVoyageClicked() {
 		buttonHoteValiderVoyage.setDisable(true);
-		int idVoyage = 12;
-		int idUtilisateur = 344;
+		
 		//buttonHoteValiderVoyage.setDisable(false);
 		Text text = new Text("Voyage validé par l'hôte");
 		text.setWrappingWidth(250);
@@ -435,8 +488,8 @@ public class Controller{
 		borderPane1.setCenter(text);
 		
 		borderPane1.setStyle("-fx-background-color: #0DFF00; -fx-background-radius: 5px;");
-		creeFichierChat(idVoyage,idUtilisateur);
-		ecrireFichierChat(idVoyage,idUtilisateur,true,"|vh|Voyage validé par l'hôte");
+		creeFichierChat(idVoyage,idVoyageur,idHote);
+		ecrireFichierChat(idVoyage,idVoyageur,idHote,true,"|vh|Voyage validé par l'hôte");
 	
 		vBoxChat.getChildren().add(borderPane);
 		vBoxChat.autosize();
@@ -449,8 +502,7 @@ public class Controller{
 	public void onRefuserVoyageClicked() {
 		buttonRefuserVoyage.setDisable(true);
 		buttonHoteValiderVoyage.setDisable(true);
-		int idVoyage = 12;
-		int idUtilisateur = 344;
+		
 		//buttonHoteValiderVoyage.setDisable(false);
 		Text text = new Text("Voyage refusé par l'hôte");
 		text.setWrappingWidth(250);
@@ -462,8 +514,8 @@ public class Controller{
 		borderPane1.setCenter(text);
 		
 		borderPane1.setStyle("-fx-background-color: #FF0000; -fx-background-radius: 5px;");
-		creeFichierChat(idVoyage,idUtilisateur);
-		ecrireFichierChat(idVoyage,idUtilisateur,true,"|rh|Voyage refusé par l'hôte");
+		creeFichierChat(idVoyage,idVoyageur,idHote);
+		ecrireFichierChat(idVoyage,idVoyageur,idHote,true,"|rh|Voyage refusé par l'hôte");
 		
 		vBoxChat.getChildren().add(borderPane);
 		vBoxChat.autosize();
